4f9dbeedac92acd4cd32f8dfae2044db25ec7d5e
Bradley Grantham <bgrantham@mac0332.channel4.local>
2017-09-19 13:47:57 +0100

    Complete decorators exercise

--- decorators.py
+++ decorators.py
@@ -1,16 +1,23 @@
 def type_check(correct_type):
-    #put code here
+    def check_type(old_func):
+        def new_func(*args, **kwargs):
+            if type(args[0]) == correct_type:
+                return old_func(args[0])
+            else:
+                return "Bad type"
+        return new_func
+    return check_type
 
 @type_check(int)
 def times2(num):
     return num*2
 
 print(times2(2))
-times2('Not A Number')
+print(times2('Not A Number'))
 
 @type_check(str)
 def first_letter(word):
     return word[0]
 
 print(first_letter('Hello World'))
-first_letter(['Not', 'A', 'String'])
+print(first_letter(['Not', 'A', 'String']))
