e3d5ad280713363540996484a82a26fe78c8f97d
Bradley Grantham <bgrantham@mac0332.local>
2017-09-19 12:03:43 +0100

    Complete closures exercise

--- closures.py
+++ closures.py
@@ -1,4 +1,11 @@
-# your code goes here
+def multiplier_of(x):
+    def multiplywith(y):
+        return x*y
+    return multiplywith
+
+
 
 multiplywith5 = multiplier_of(5)
-multiplywith5(9)
+multiplywith4 = multiplier_of(4)
+print(multiplywith4(9))
+print(multiplywith5(9))
