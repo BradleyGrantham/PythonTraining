54376bc7d56f657adb5c595cdff90fcbc9e25d60
Bradley Grantham <bgrantham@mac0332.channel4.local>
2017-09-21 14:13:02 +0100

    Add graphs for ICA

---
+++ sklearn/ex4/feature_agglomeration.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+from sklearn import datasets, cluster
+from sklearn.feature_extraction.image import grid_to_graph
+import matplotlib.pyplot as plt
+
+digits = datasets.load_digits()
+images = digits.images
+
+X = np.reshape(images, (len(images), -1))
+connectivity = grid_to_graph(*images[0].shape)
+
+agglo = cluster.FeatureAgglomeration(connectivity=connectivity,
+                                     n_clusters=32)
+agglo.fit(X)
+X_reduced = agglo.transform(X)
+X_approx = agglo.inverse_transform(X_reduced)
+images_approx = np.reshape(X_approx, images.shape)
+
+'''
+Agglo tranform reduces the dimensionality by finding similar features
+
+So I believe this particular one will reduce the number of features to ~ 32
+'''
