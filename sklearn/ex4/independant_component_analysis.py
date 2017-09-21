f56e2028c9b6222b4ad0e7799c5b07800c6099fd
Bradley Grantham <bgrantham@mac0332.channel4.local>
2017-09-21 14:12:45 +0100

    Complete independant component analysis

--- sklearn/ex4/independant_component_analysis.py
+++ sklearn/ex4/independant_component_analysis.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn import decomposition
 from scipy import signal
+import matplotlib.pyplot as plt
 
 time = np.linspace(0, 10, 2000) # the 3rd argument is how many numbers you want between the first two arguments
 
@@ -23,7 +24,17 @@ ica = decomposition.FastICA()
 S_ = ica.fit_transform(X)
 
 A_ = ica.mixing_.T
+
+# all close returns true if two arrays are the same elementwise to a specified tolerance
 print np.allclose(X, np.dot(S_, A_) + ica.mean_)
+
+
+plt.figure()
+plt.plot(time, s1, c='blue')
+plt.plot(time, s2, c='green')
+plt.plot(time, s3, c='red')
+plt.savefig('original_signals.png')
+
 
 '''
 Most of this has gone completely over my head
